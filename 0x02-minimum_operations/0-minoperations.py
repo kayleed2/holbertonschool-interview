dc2ee6b64b8e0f309c4789fc6e513a293572562a
Kaylee Dao <kayleed2@gmail.com>
2022-09-21 13:16:45 -0500

    Adding repo

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -16,27 +16,20 @@ def minOperations(n):
 
     dp = [sys.maxsize] * (n + 1)
 
-    # Initial state
     dp[1] = 0
 
-    # Iterate for the remaining numbers
     for i in range(2, n + 1):
 
-        # If the number can be obtained
-        # by multiplication by 2
         if i % 2 == 0:
             x = dp[i // 2]
             if x + 1 < dp[i]:
                 dp[i] = x + 1
 
-        # If the number can be obtained
-        # by multiplication by 3
         if i % 3 == 0:
             x = dp[i // 3]
             if x + 1 < dp[i]:
                 dp[i] = x + 1
 
-        # Obtaining the number by adding 1
         x = dp[i - 1]
         if x + 1 < dp[i]:
             dp[i] = x + 1
