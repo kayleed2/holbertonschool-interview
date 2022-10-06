af6c72d33cc9481e40b1d94f8b15a8ddd4ba0002
Kaylee Dao <kayleed2@gmail.com>
2022-10-06 15:11:20 -0500

    Pushing repo

--- 0x03-sandpiles/0-sandpiles.c
+++ 0x03-sandpiles/0-sandpiles.c
@@ -20,28 +20,4 @@ void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 			grid1[row][col] += grid2[row][col];
 		}
 	}
-    print_grid(grid1);
-
-}
-
-/**
- * print_grid - Print 3x3 grid
- *
- * @grid: 3x3 grid
- *
- */
-void print_grid(int grid[3][3])
-{
-	int i, j;
-
-	for (i = 0; i < 3; i++)
-	{
-		for (j = 0; j < 3; j++)
-		{
-			if (j)
-				printf(" ");
-			printf("%d", grid[i][j]);
-		}
-		printf("\n");
-	}
 }
--- 0x03-sandpiles/sandpiles.h
+++ 0x03-sandpiles/sandpiles.h
@@ -3,6 +3,5 @@
 
 
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
-static void print_grid(int grid[3][3]);
 
 #endif
