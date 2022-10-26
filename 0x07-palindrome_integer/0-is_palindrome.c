65145514dc3a01a2374c2e602b3b9fee845ad4db
Kaylee Dao <kayleed2@gmail.com>
2022-10-26 09:32:04 -0500

    Is Palindrome using reversed

---
+++ 0x07-palindrome_integer/0-is_palindrome.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "palindrome.h"
+
+
+int is_palindrome(unsigned long n)
+{
+	unsigned long reversed, remainder;
+	unsigned long original = n;
+
+    if (!n)
+        return (0);
+
+	while (n != 0)
+	{
+		remainder = n % 10;
+		reversed = reversed * 10 + remainder;
+		n /= 10;
+	}
+
+	if (original == reversed)
+		return (1);
+	
+	return (0);
+}
