e4ba848063f58d23143fcf80aba5ebd0e0d6b240
Kaylee Dao <kayleed2@gmail.com>
2022-12-08 10:33:19 -0600

    Betty

--- 0x0A-menger/0-menger.c
+++ 0x0A-menger/0-menger.c
@@ -7,24 +7,25 @@
  */
 void menger(int level)
 {
-    int row, col, size;
+int row, col, size;
 
-    size = pow(3, level);
+size = pow(3, level);
 
-    if (level == 0) 
-    {
-            printf("%c", '#');
-    } else if (level > 0)
-    {
-        for (col = 0; col < size; col++)
-        {
-            for (row = 0; row < size; row++)
-            {
-               printf("%c", (print(row, col)));
-            }
-            printf("\n");
-        }
-    }
+if (level == 0)
+{
+printf("%c", '#');
+}
+else if (level > 0)
+{
+for (col = 0; col < size; col++)
+{
+for (row = 0; row < size; row++)
+{
+printf("%c", (print(row, col)));
+}
+printf("\n");
+}
+}
 }
 
 /**
@@ -32,16 +33,17 @@ void menger(int level)
  *
  * @row: row
  * @col: col
+ * Return: print
  */
 
 char print(int row, int col)
 {
-    while (row || col)
-	{
-		if (row % 3 == 1 && col % 3 == 1)
-			 return(' ');
-		row /= 3;
-		col /= 3;
-	}
-	return('#');
+while (row || col)
+{
+if (row % 3 == 1 && col % 3 == 1)
+return (' ');
+row /= 3;
+col /= 3;
+}
+return ('#');
 }
