754f8850c610c092370c480b780413d1bf8187c5
Kaylee Dao <kayleed2@gmail.com>
2023-01-06 08:24:55 -0600

    Walls

---
+++ 0x00-rain/0-rain.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+0_rain
+"""
+
+def rain(walls):
+    """how much water is being retained by the walls"""
+    if walls == None:
+        return 0
+
+    sum = 0
+    for i in range(1, len(walls) - 1):
+        left = walls[i]
+        for j in range(i):
+            left = max(left, walls[j])
+        right = walls[i]
+        for j in range(i + 1, len(walls)):
+            right = max(right, walls[j])
+        sum = sum + (min(left, right) - walls[i])
+    return sum
