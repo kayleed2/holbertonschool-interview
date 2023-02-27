673af2540f8d16921b3e2b99518ad31de545534a
Kaylee Dao <kayleed2@gmail.com>
2023-02-27 12:06:37 -0600

    Make change

--- 0x0F-making_change/0-making_change.py
+++ 0x0F-making_change/0-making_change.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+# Given a pile of coins of 
+# different values, determine 
+# the fewest number of coins needed to 
+# meet a given amount total.
 
 def makeChange(coins, total):
     # If total is 0 or less, return 0
