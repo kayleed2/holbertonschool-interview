1f53e1cf31baf4e3bdafd29e5e9d6d23548f318b
Kaylee Dao <kayleed2@gmail.com>
2023-02-27 12:11:03 -0600

    Make change

--- 0x0F-making_change/0-making_change.py
+++ 0x0F-making_change/0-making_change.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-"""Given a pile of coins of different values, determine 
-the fewest number of coins needed to 
+"""Given a pile of coins of different values, determine
+the fewest number of coins needed to
 meet a given amount total. """
+
 
 def makeChange(coins, total):
     # If total is 0 or less, return 0
@@ -9,20 +10,20 @@ def makeChange(coins, total):
         return 0
     
     # Initialize the array to store the minimum number of coins needed to make each amount
-    dp = [float('inf')] * (total + 1)
+    minCoin = [float('inf')] * (total + 1)
     # The minimum number of coins needed to make 0 is 0
-    dp[0] = 0
-    
+    minCoin[0] = 0
+
     # Loop through all the coin values
     for coin in coins:
         # Loop through all the amounts starting from the coin value
         for i in range(coin, total + 1):
             # Calculate the minimum number of coins needed to make the amount
-            dp[i] = min(dp[i], dp[i - coin] + 1)
-    
+            minCoin[i] = min(minCoin[i], minCoin[i - coin] + 1)
+
     # If total cannot be met by any number of coins you have, return -1
-    if dp[total] == float('inf'):
+    if minCoin[total] == float('inf'):
         return -1
-    
+
     # Return the fewest number of coins needed to meet total
-    return dp[total]
+    return minCoin[total]
