41b6b2793294d7dc96d3e85d11a6cd2a6d9c651d
Kaylee Dao <kayleed2@gmail.com>
2023-03-13 09:51:33 -0500

    Prime

--- 0x1C-primegame/0-prime_game.py
+++ 0x1C-primegame/0-prime_game.py
@@ -5,7 +5,6 @@
 def isWinner(x, nums):
     """Finds the Winner"""
     def isPrime(n):
-        """Determines prime number"""
         if n < 2:
             return False
         for i in range(2, int(n**0.5)+1):
@@ -13,26 +12,25 @@ def isWinner(x, nums):
                 return False
         return True
 
-    def play(n):
+    def play(n, current_player):
         """Determines current gameplay"""
         primes = [i for i in range(2, n+1) if isPrime(i)]
-        current_player = 'Maria'
-        while primes:
-            move = None
-            for p in primes:
-                if n % p == 0:
-                    move = p
-                    break
-            if move is None:
-                break
-            multiples = [i for i in range(move, n+1, move)]
-            primes = [p for p in primes if p not in multiples]
-            current_player = 'Ben' if current_player == 'Maria' else 'Maria'
-        return current_player
+        if not primes:
+            return current_player  # no primes left, current player loses
+        for p in primes:
+            multiples = [i for i in range(p, n+1, p)]
+            if current_player == 'Maria':
+                next_player = 'Ben'
+            else:
+                next_player = 'Maria'
+            result = play(n - len(multiples), next_player)
+            if result == current_player:
+                return current_player  # found a winning move
+        return next_player  # no winning moves, next player wins
 
     scores = {'Maria': 0, 'Ben': 0}
     for n in nums:
-        winner = play(n)
+        winner = play(n, 'Maria')
         if winner is not None:
             scores[winner] += 1
 
